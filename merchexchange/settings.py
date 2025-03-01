ac2b9c5a803fe47db5de9916711919415f2742a9
Cody Keener <36174154+codyKeener@users.noreply.github.com>
2025-03-01 12:42:10 -0600

    Completely removed djmoney and deleted the field in the listing table for price_currency

--- merchexchange/settings.py
+++ merchexchange/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'merchexchangeapi',
-    'djmoney'
+    'merchexchangeapi'
 ]
 
 CORS_ORIGIN_WHITELIST = (
