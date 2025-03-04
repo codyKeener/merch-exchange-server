0bac3c64e036b66f0c12b0705414263c07682df1
Cody Keener <36174154+codyKeener@users.noreply.github.com>
2025-03-03 22:30:37 -0600

    Added fixtures for categories and updated auth.py to work with user fields

--- merchexchangeapi/models/user.py
+++ merchexchangeapi/models/user.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
 
-    username = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
