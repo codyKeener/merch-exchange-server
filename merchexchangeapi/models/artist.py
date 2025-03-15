b11ed2e23a464f2c3a5924ac414a0e7c4c03c4a9
Cody Keener <36174154+codyKeener@users.noreply.github.com>
2025-03-15 00:28:08 -0500

    Added artist fixtures and updated artist ordering to be alphabetical

--- merchexchangeapi/models/artist.py
+++ merchexchangeapi/models/artist.py
@@ -2,8 +2,11 @@ from django.db import models
 
 class Artist(models.Model):
 
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     genre = models.CharField(max_length=50)
+    
+    class Meta:
+        ordering = ("name", )
 
     def __str__(self):
         return self.name
--- merchexchangeapi/models/category.py
+++ merchexchangeapi/models/category.py
@@ -4,6 +4,6 @@ class Category(models.Model):
 
     label = models.CharField(max_length=50)
     image = models.URLField()
-
+    
     def __str__(self):
         return self.label
